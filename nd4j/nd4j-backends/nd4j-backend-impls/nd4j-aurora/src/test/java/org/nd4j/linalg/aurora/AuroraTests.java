dc6fa2e79e87cd6b099d394ffd2e0407a9853716
Adam Gibson <1144306+agibsonccc@users.noreply.github.com>
2020-08-30 08:51:14 +0900

    Add test

---
+++ nd4j/nd4j-backends/nd4j-backend-impls/nd4j-aurora/src/test/java/org/nd4j/linalg/aurora/AuroraTests.java
@@ -0,0 +1,25 @@
+package org.nd4j.linalg.aurora;
+
+import org.junit.Test;
+import org.nd4j.nativeblas.Nd4jAurora;
+
+import static org.nd4j.nativeblas.Nd4jAurora.*;
+
+public class AuroraTests {
+
+    @Test
+    public void testAuroraBasic() {
+        veo_proc_handle proc = veo_proc_create(0);
+        Nd4jAurora.veo_args argp = veo_args_alloc();
+        veo_thr_ctxt ctx = veo_context_open(proc);
+        String libPath = System.getenv("LIB_PATH");
+        String methodName = System.getenv("METHOD_NAME");
+        long handle = veo_load_library(proc, libPath);
+        veo_call_async_by_name(ctx, handle, methodName, argp);
+        veo_args_free(argp);
+        veo_context_close(ctx);
+        veo_proc_destroy(proc);
+    }
+
+
+}
